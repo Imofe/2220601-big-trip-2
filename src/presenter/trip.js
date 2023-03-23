6867233af7b7d58ff34444310d6e72d821c95467
Roman <romanlabzin03@gmail.com>
2023-03-23 15:34:23 +0500

    1.10 fixed

--- src/presenter/trip.js
+++ src/presenter/trip.js
@@ -1,6 +1,5 @@
 import { render, RenderPosition } from '../render';
 import PointEditView from '../view/point-edit';
-// import PointNewView from '../view/point-new';
 import PointView from '../view/point';
 import SortView from '../view/sort';
 import TripList from '../view/trip-list';
@@ -15,11 +14,10 @@ export default class Trip {
   init() {
     render(new SortView(), this.container, RenderPosition.BEFOREEND);
     render(this.component, this.container);
-    // render(new PointNewView, this.component.getElement(), RenderPosition.BEFOREEND);
     render(new PointEditView(), this.component.getElement(), RenderPosition.BEFOREEND);
 
     for (let i = 0; i < WAYPOINT_COUNT; i++) {
-      render(new PointView(), this.component.getElement(), RenderPosition.AFTERBEGIN.BEFOREEND);
+      render(new PointView(), this.component.getElement(), RenderPosition.BEFOREEND);
     }
   }
 }
